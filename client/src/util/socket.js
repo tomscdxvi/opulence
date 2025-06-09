c94bd0661ba667ca3d1ce0662a5d1edb27ed181d
tomscdxvi <txmmychiu@gmail.com>
2025-06-09 13:18:36 -0400

    Fix production site

--- client/src/util/socket.js
+++ client/src/util/socket.js
@@ -1,4 +1,6 @@
 // socket.js
 import { io } from 'socket.io-client';
 
-export const socket = io(import.meta.env.VITE_SOCKET_URL);
+export const socket = io(import.meta.env.VITE_SOCKET_URL, {
+    transports: ["websocket", "polling"],
+});
